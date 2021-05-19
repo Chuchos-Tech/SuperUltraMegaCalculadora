8a0ff77c3bedb0b8fb54c54fe0a517c5a02d06ad
Daeli-Smile <55467390+Daeli-Smile@users.noreply.github.com>
2021-05-19 11:44:21 -0500

    Add files via upload

---
+++ Calculadora GUI(ora si)/src/cApp.cpp
@@ -0,0 +1,23 @@
+#include "cApp.h"
+
+wxIMPLEMENT_APP(cApp);
+
+cApp::cApp(){
+    //ctor
+}
+
+cApp::~cApp(){
+    //dtor
+}
+
+bool cApp::OnInit(){
+    m_frame1 = new cMain();
+    wxImage::AddHandler(new wxPNGHandler());
+    m_frame1 -> SetIcon({"Resources/icon.png", wxBITMAP_TYPE_PNG});
+    m_frame1 -> Show();
+    HWND hide;
+    AllocConsole();
+    hide = FindWindowA("ConsoleWindowClass", NULL);
+    ShowWindow(hide, 0);
+    return true;
+}
